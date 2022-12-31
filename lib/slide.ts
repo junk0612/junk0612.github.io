e6bc6d432e6759f7aec8f78d4210403cc9d8a1f9
Junichi Kobayashi <nnujabok@gmail.com>
2023-01-01 02:23:06 +0900

    Enable to make slides

---
+++ lib/slide.ts
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react'
+
+export type Slide = {
+  title: string
+  published: string
+  tags: string[]
+  slug: string
+}
+
+export type SlideWithContent = Slide & { content: string }
+
+export const slides: Slide[] = [
+  /*
+  {
+    title: 'Blog つくりました',
+    published: '2022/06/24',
+    tags: ['JS', 'Next.js', 'Rust'],
+    slug: 'new-blog',
+  },
+  */
+]
+
+export const useSlide = () => {
+  const [Reveal, setReveal] = useState<RevealStatic>()
+  const [RevealMarkdown, setRevealMarkdown] = useState<Plugin>()
+  const [Highlight, setHighlight] = useState<Plugin>()
+
+  useEffect(() => {
+    const clientSideInitialization = async () => {
+      if (Reveal == null) {
+        setReveal(await (await import('reveal.js')).default)
+      } else if (RevealMarkdown == null) {
+        setRevealMarkdown(
+          await (
+            await import('reveal.js/plugin/markdown/markdown.esm')
+          ).default
+        )
+      } else if (Highlight == null) {
+        setHighlight(
+          await (
+            await import('reveal.js/plugin/highlight/highlight.esm')
+          ).default
+        )
+      } else {
+        await Reveal.initialize({
+          plugins: [RevealMarkdown, Highlight],
+          embedded: true,
+          shuffle: false,
+          history: true,
+        })
+      }
+    }
+    clientSideInitialization()
+  }, [Reveal, RevealMarkdown, Highlight])
+}
---
+++ lib/types/reveal.d.ts
@@ -0,0 +1,14 @@
+declare module 'reveal.js' {
+  const content: RevealStatic & { destroy: () => void }
+  export default content
+}
+
+declare module 'reveal.js/plugin/markdown/markdown.esm' {
+  const content: RevealMarkdown
+  export default content
+}
+
+declare module 'reveal.js/plugin/highlight/highlight.esm' {
+  const content: RevealHighlight
+  export default content
+}
